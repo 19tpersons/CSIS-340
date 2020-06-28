5ee145f704788ff38dc4b7a7edcf4c279e70148f
Ronni <ne8979kq@go.minnstate.edu>
2020-06-28 11:04:04 -0500

    changed some labels, created the PrintModal class

---
+++ DeckEstimator/src/deckpackage/modal/PrintModal.java
@@ -0,0 +1,12 @@
+package deckpackage.modal;
+
+import java.util.HashMap;
+
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class PrintModal {
+	private HashMap<String, String> breakdown = new HashMap<String, String>();
+	
+	
+}
